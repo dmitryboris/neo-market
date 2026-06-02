0c4c6ada9adcba449120438504fc742f85835d46
mitya <dmitrij_bo05@mail.ru>
2026-06-02 16:12:08 +0500

    fix: ProductResponse

--- b2b/src/schemas/product.py
+++ b2b/src/schemas/product.py
@@ -5,24 +5,29 @@ from typing import List, Optional
 from src.models.product import ProductStatus
 from src.schemas.sku import SKUResponse
 
+
 class ProductImageResponse(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     id: UUID
     url: str
     ordering: int
 
+
 class ProductImageCreate(BaseModel):
     url: str = Field(..., description="Ссылка на изображение")
     ordering: int = Field(..., ge=0, description="Порядок отображения")
+
 
 class ProductCharacteristicResponse(BaseModel):
     id: UUID
     name: str
     value: str
 
+
 class ProductCharacteristicCreate(BaseModel):
     name: str = Field(..., min_length=1, max_length=100)
     value: str = Field(..., min_length=1, max_length=500)
+
 
 class ProductCreateRequest(BaseModel):
     category_id: Optional[str] = None
@@ -31,11 +36,13 @@ class ProductCreateRequest(BaseModel):
     images: List[ProductImageCreate] | None = None
     characteristics: Optional[List[ProductCharacteristicCreate]] = Field(default_factory=list)
 
+
 class ProductUpdateRequest(BaseModel):
     category_id: Optional[UUID] = None
     title: Optional[str] = Field(None, min_length=1, max_length=255)
     description: Optional[str] = Field(None, min_length=1, max_length=5000)
     status: Optional[str] = None
+
 
 class ProductResponse(BaseModel):
     model_config = ConfigDict(from_attributes=True)
@@ -43,21 +50,28 @@ class ProductResponse(BaseModel):
     seller_id: UUID
     category_id: UUID
     title: str
-    description: str | None = None
+    slug: str
+    description: str
     status: ProductStatus
+    deleted: bool
+    blocking_reason_id: UUID | None
+    moderator_comment: str | None
     images: List[ProductImageResponse]
     characteristics: List[ProductCharacteristicResponse]
     skus: List[SKUResponse]
     created_at: datetime
     updated_at: datetime
 
+
 class ProductImageUpdateRequest(BaseModel):
     url: Optional[str] = None
     ordering: Optional[int] = Field(None, ge=0)
 
+
 class ProductImageCreateRequest(BaseModel):
     url: str
     ordering: int = 0
+
 
 class ProductShortResponse(BaseModel):
     model_config = ConfigDict(from_attributes=True)
@@ -69,6 +83,7 @@ class ProductShortResponse(BaseModel):
     created_at: datetime
     min_price: int | None = None
     cover_image: str | None = None
+
 
 class ProductPaginatedResponse(BaseModel):
     total_count: int
